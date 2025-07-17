728d708ba86be665f7cd4c871440052b2794b89d
Siddhant Gawad <siddhantgawad4@gmail.com>
2025-07-17 08:51:59 -0400

    folder change did some recoil work

--- state-lift-vs-context-api/context-api/color-tag-manager/src/context/TagContext.ts
+++ state-lift-vs-context-api/context-api/color-tag-manager/src/context/TagContext.ts
@@ -2,7 +2,7 @@ import { createContext } from "react";
 
 type tagProps={
     tag:string|null;
-    setTag:(newTag: string)=>void;
+    tagFnctn:(newTag: string)=>void;
 }
 
 export const TagContext = createContext<tagProps|null>(null);
